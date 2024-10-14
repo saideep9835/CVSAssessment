58a9754ccc762fa80d00d19f4e8664d4728a0304
Saideep9835 <tsaideep98@gmail.com>
2024-10-14 12:42:27 -0500

    Initial Commit

---
+++ FlickrInterview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlickrInterview
+//
+//  Created by Saideep Reddy Talusani on 10/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlickrInterview/FlickrInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrInterviewApp.swift
+//  FlickrInterview
+//
+//  Created by Saideep Reddy Talusani on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
